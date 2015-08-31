2ece5e45eb59eac9febdf766de7782c733f90b9c
Daniel Burgner <daniel.burgner@dominionenterprises.com>
2015-08-31 13:35:05 -0400

    Factorial solution - Daniel.

--- factorial.php
+++ factorial.php
@@ -1,6 +1,12 @@
 <?php
+/**
+ factorial.php
+ Daniel Burgner PHP 101
+ August 31, 2015
+ */
 
 $input = isset($argv[1]) ? $argv[1] : die("No input given\n");
+$k = isset($argv[2]) ? $argv[2] : 0;
 
 /**
  * Exercise 3 - Factorial
@@ -12,5 +18,23 @@ $input = isset($argv[1]) ? $argv[1] : die("No input given\n");
  *
  *
  * Bonus: Use your factorial function to solve k-permutations of n
- * (k n) = n! / ((n - k)! * k!)
+ * (k n) = n! / ((n - k)!)
  */
+function factorial($n) {
+	if ($n < 2) {
+		return 1;
+	}	
+	if ($n >= 2) {
+		return ($n * factorial($n - 1));
+	}
+}
+
+function permutation($n, $k) {
+	return factorial($n)/(factorial($n - $k));	
+}
+
+$nFactorial = factorial($input);
+$nPermutation = permutation($input, $k);
+
+print $input . ' factorial is: ' . $nFactorial . PHP_EOL;
+print $k . ' permutations of ' . $nFactorial . ' is: ' . $nPermutation . PHP_EOL;
